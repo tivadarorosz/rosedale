36e29b0429e347d7bcdef857a8af3d774516663a
Tivadar Orosz <tivadar@tivadarorosz.com>
2024-11-13 18:45:46 +0000

    Update app.py to enforce HTTPS using Flask-Talisman

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask
+from flask_talisman import Talisman
 from customers import customers
 from transactions import transactions
 from appointments import appointments
 from square_api import square_api
 
 app = Flask(__name__)
+
+# Enforce HTTPS
+Talisman(app)
 
 @app.route("/")
 def home():
